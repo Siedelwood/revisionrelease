12c4addb16d0a64b7c252cfcf8046a5a1a9d281d
totalwarANGEL <n3crosh8d3@gmail.com>
2022-12-19 14:22:29 +0100

    Migrade shp salesman module

--- release/modules/QSB_2_NewBehavior/qsb_2_newbehavior.lua
+++ release/modules/QSB_2_NewBehavior/qsb_2_newbehavior.lua
@@ -382,10 +382,10 @@ You may use and modify this file unter the terms of the MIT licence.
 
 -- -------------------------------------------------------------------------- --
 
---
+---
 -- Dieses Modul fügt neue Behavior für den Editor hinzu.
 --
--- <b>Vorausgesetzte Module:</b>
+-- <p><b>Vorausgesetzte Module:</b></p>
 -- <ul>
 -- <li><a href="qsb.html">(0) Basismodul</a></li>
 -- <li><a href="modules.QSB_1_GuiControl.QSB_1_GuiControl.html">(1) Anzeigekontrolle</a></li>
